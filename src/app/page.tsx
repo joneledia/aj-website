923cd6db7ba9e112859462c29d379c11e7b71190
joneledia <eledia.jon@gmail.com>
2024-07-14 15:41:54 +0800

    Update types

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -265,7 +265,7 @@ export default function Home() {
       </main>
       <footer className="flex flex-col gap-2 sm:flex-row py-6 w-full shrink-0 items-center px-4 md:px-6 border-t">
         <p className="text-xs text-muted-foreground">
-          &copy; 2024 Acme Petroleum. All rights reserved.
+          &copy; 2024 AJ Lubricants. All rights reserved.
         </p>
         <nav className="sm:ml-auto flex gap-4 sm:gap-6">
           <Link
@@ -288,7 +288,7 @@ export default function Home() {
   );
 }
 
-function FuelIcon(props) {
+function FuelIcon(props: React.SVGProps<SVGSVGElement>) {
   return (
     <svg
       {...props}
@@ -310,7 +310,7 @@ function FuelIcon(props) {
   );
 }
 
-function XIcon(props) {
+function XIcon(props: React.SVGProps<SVGSVGElement>) {
   return (
     <svg
       {...props}
